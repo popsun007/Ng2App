74b494b307a9039d0eb09e1f7d7089db84826139
edison wang <edison7476@hotmail.com>
2017-07-22 18:48:17 -0700

    Add some materials examples

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-serve></app-serve>
+  //   <app-serve></app-serve>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
